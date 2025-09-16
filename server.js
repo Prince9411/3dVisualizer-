52ebd41e4314c52a70ba074a3b0fee1c47e2731f
Prince Rana <118838109+Prince9411@users.noreply.github.com>
2025-09-16 10:09:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -85,5 +85,7 @@ app.get("/api/insert-sample", async (req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+
+
 
